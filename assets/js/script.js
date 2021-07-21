85e0b3cfbde5b05bf228bf5747952b84f2c36436
JuCSMartin <julianacmsantos@gmail.com>
2021-07-21 13:44:35 -0300

    Implementação das funções 'validaMensagem' e 'enviar'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let mensagem = document.querySelector('#mensagem')
+let nomeOk = false
+let emailOK = false
+let mensagemOK = false
 
 nome.style.width = '100%'
 email.style.width = '100%'
@@ -14,6 +17,7 @@ function validaNome() {
     }else{
         txtNome.innerHTML = 'Nome válido'
         txtNome.style.color = 'green'
+        nomeOk = true
     }
 }
 
@@ -25,5 +29,28 @@ function validaEmail() {
     }else{
         txtEmail.innerHTML = "E-mail válido"
         txtEmail.style.color = 'green'
+        emailOk = true
     }
 }
+
+function validaMensagem() {
+    let txtMensagem = document.querySelector('#txtMensagem')
+    if (mensagem.value.length >= 100) {
+        txtMensagem.innerHTML = 'Texto muito grande. Digite no máximo 100 caracteres.'
+        txtMensagem.style.color = 'red'
+        txtMensagem.style.display = 'block'
+    }else{
+        txtMensagem.style.display = 'none'
+        mensagemOk = true
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && mensagemOk == true) {
+        alert ('Prontinho! Você receberá um pack de fotos novinho em breve!')
+    }else{
+        alert ('Preencha o formulário corretamente antes de enviar...')
+    }
+}
+
+
